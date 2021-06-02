96696ec628975f8d9ef02fd01e4cf1aece9d24f1
ahmad-ahmad <aa0796780751@gmail.com>
2021-06-02 21:46:35 +0300

    chat_server.js

--- src/server.js
+++ src/server.js
@@ -15,11 +15,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
+const users = {} //users for chat
+
+/////////////////////////////////////////////
+//////////// connection ////////////////////
+///////////////////////////////////////////
 io.on('connection', socket => {
   console.log('finally i am connceted !!!');
   let payload = 'ibrahim banat';
   io.emit('front', payload);
+  socket.emit('chat_message','Hello');
+  socket.on('send_chat_message', message=>{
+   console.log(message);
+  });
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
 });
+/////////////////////////////////////////////
+//////////// connection ////////////////////
+///////////////////////////////////////////
 
 module.exports = port => {
   server.listen(port, () => {
@@ -28,14 +51,4 @@ module.exports = port => {
   });
 };
 
-/**
- * handling spotify
- *
- */
 
-/**
- *
- *
- * chat room functionality
- *
- */
